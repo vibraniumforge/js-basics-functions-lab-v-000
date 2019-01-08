d297c41685eb8f8ca8d0c28c764ebf01ef69e750
Greg Morgan <gregm9999@gmail.com>
2019-01-08 21:44:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function calculatesFarePrice(start, destination) {
   if (distanceTravelledInFeet(start, destination)  < 400) {
     return 0;
   }
-  if (distanceTravelledInFeet(start, destination)  > 400 && 
+  if (distanceTravelledInFeet(start, destination)  > 400 &&
   distanceTravelledInFeet(start, destination)  < 2000) {
-    return distanceTravelledInFeet(start, destination)  * .02;
+    return (distanceTravelledInFeet(start, destination) -400) * .02;
   }
   if (distanceTravelledInFeet(start, destination)  > 2000 && distanceTravelledInFeet(start, destination)  < 2500) {
     return 25;
